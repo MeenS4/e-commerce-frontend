07d86ba9ef41c76c6a1ec6aa014cfd78cd9b4784
MeenS4 <rokoszoczek@gmail.com>
2024-06-25 13:50:01 +0200

    refactor / add isFullWidth props to divider component

--- src/libs/shared/components/divider/divider.tsx
+++ src/libs/shared/components/divider/divider.tsx
@@ -1,10 +1,14 @@
 import { Grid } from "../grid";
 import styles from "./divider.module.scss";
 
-export const Divider = () => {
-  return (
-    <Grid>
-      <div className={styles["divider"]} />
-    </Grid>
-  );
+export const Divider = ({ isFullWidth }: { isFullWidth?: boolean }) => {
+  if (isFullWidth) {
+    return <div className={styles["divider"]} />;
+  } else {
+    return (
+      <Grid>
+        <div className={styles["divider"]} />
+      </Grid>
+    );
+  }
 };
