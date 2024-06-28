ebe4ba1db2a74723ac5ee1e701915ee373eff92b
MeenS4 <rokoszoczek@gmail.com>
2024-06-28 11:49:26 +0200

    feat / add onClick prop to Icon component

--- src/libs/shared/components/icon/icon.tsx
+++ src/libs/shared/components/icon/icon.tsx
@@ -2,10 +2,23 @@ import classNames from "classnames";
 import styles from "./icon.module.scss";
 import { IconProps } from "./icon.types";
 
-export const Icon = ({ src, alt, className }: IconProps) => {
-  const iconClassNames = classNames(styles["icon"], className);
+export const Icon = ({ src, alt, onClick, className }: IconProps) => {
+  const iconClassNames = classNames(styles["icon"], className, {
+    [styles["icon--clickable"]]: onClick,
+  });
 
-  return (
-    <img className={iconClassNames} src={`assets/icons/${src}`} alt={alt} />
-  );
+  if (onClick !== null) {
+    return (
+      <img
+        className={iconClassNames}
+        src={`assets/icons/${src}`}
+        alt={alt}
+        onClick={onClick}
+      />
+    );
+  } else {
+    return (
+      <img className={iconClassNames} src={`assets/icons/${src}`} alt={alt} />
+    );
+  }
 };
--- src/libs/shared/components/icon/icon.types.ts
+++ src/libs/shared/components/icon/icon.types.ts
@@ -1,1 +1,8 @@
-export type IconProps = { src: string; alt: string; className?: string };
+import { MouseEventHandler } from "react";
+
+export type IconProps = {
+  src: string;
+  alt: string;
+  onClick?: MouseEventHandler<HTMLElement>;
+  className?: string;
+};
